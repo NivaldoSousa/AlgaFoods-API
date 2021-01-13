0c45310a02b3c5401d8c1f672a87b7b98f8d7cf0
NGA Sousa <nillsousa026@gmail.com>
2021-01-12 22:26:44 -0300

    Acertos do projeto

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -3,9 +3,8 @@ package com.algaworks.algafood.api.controller;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,9 +15,14 @@ public class CozinhaController {
     @Autowired
     private CozinhaRepository cozinhaRepository;
 
-    @GetMapping
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Cozinha> listar(){
         return cozinhaRepository.listar();
+    }
+
+    @GetMapping("/{cozinhaId}")
+    public Cozinha buscar(@PathVariable("cozinhaId")Long id){
+        return cozinhaRepository.buscar(id);
     }
 
 }
--- algafood-api/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -1,4 +1,6 @@
 package com.algaworks.algafood.domain.model;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -6,7 +8,7 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.Objects;
-
+@JsonRootName("gastronomia")
 @Data // essa antaçao do lombok possui anotaçoes já inclusas como: @Getter, @Setter, @ToString, @EqualsAndHashCode
 @EqualsAndHashCode(onlyExplicitlyIncluded = true) /*essa propriedade do EqualsAndHashCode diz que somente atributo
 que for escolher explicitamente sera incluso */
@@ -18,6 +20,7 @@ public class Cozinha {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @JsonProperty("titulo")
     private String nome;
 
 }
