40409359dd42d1b479bc27b0ad0a301e92d13a88
nills <nillsousa026@gmail.com>
2021-02-15 22:06:02 -0300

    Mapeando relacionamento ManyToMany

--- algafood-api/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -8,26 +8,32 @@ import lombok.Setter;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
-
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 public class Restaurante {
 
-    @EqualsAndHashCode.Include
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @NotNull
-    private String nome;
+	@NotNull
+	private String nome;
 
-    @Column(name = "taxa_frete", nullable = false)
-    private Double taxaFrete;
+	@Column(name = "taxa_frete", nullable = false)
+	private Double taxaFrete;
 
-    @ManyToOne
-    @JoinColumn(name = "cozinha_id") // caso queira muda o nome da coluna quando tem relacionamento entre entidade
-    private Cozinha cozinha;
+	@ManyToOne
+	@JoinColumn(name = "cozinha_id") // caso queira muda o nome da coluna quando tem relacionamento entre entidade
+	private Cozinha cozinha;
+
+	@ManyToMany
+	@JoinTable(name = "restaurante_forma_pagamento",
+	joinColumns = @JoinColumn(name = "restaurante_id"), 
+	inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
+	private List<FormaPagamento> formasPagamento = new ArrayList<>();
 }
-
