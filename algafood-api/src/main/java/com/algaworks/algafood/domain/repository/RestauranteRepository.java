e10d12b75f5429c706443fc4ebbfa160b5a15376
nills <nillsousa026@gmail.com>
2021-02-23 10:50:35 -0300

    Alterando fetching para Eager Loading, configurando Pool Hikari

--- algafood-api/src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -3,6 +3,7 @@ package com.algaworks.algafood.domain.repository;
 import com.algaworks.algafood.domain.model.Restaurante;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
@@ -12,6 +13,9 @@ import java.util.Optional;
 public interface RestauranteRepository extends CustomJpaRepository<Restaurante, Long>, RestauranteRepositoryQueries,
 		JpaSpecificationExecutor<Restaurante> {
 
+	@Query("from Restaurante r join fetch r.cozinha join fetch r.formasPagamento")
+	List<Restaurante> findAll();
+	
 	List<Restaurante> findByTaxaFreteBetween(Double taxaInicial, Double taxaFinal);
 
 	// Consulata em JPQL utilizando uma query normal, como se fosse feita direto no
