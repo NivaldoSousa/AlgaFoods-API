4f0db1c9e93a4b4273718aabadc669966c52ec3f
nills <nillsousa026@gmail.com>
2021-03-03 11:49:41 -0300

    Implementando exception handler

--- algafood-api/src/main/java/com/algaworks/algafood/domain/exception/NegocioException.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/exception/NegocioException.java
@@ -1,9 +1,5 @@
 package com.algaworks.algafood.domain.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.BAD_REQUEST) //retorna um erro de nao encontrado
 public class NegocioException extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
