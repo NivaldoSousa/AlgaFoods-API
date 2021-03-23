e4db813f940dfb6db9267345848232b92c877a66
Nivaldo Sousa <nillsousa026@gmail.com>
2021-03-23 09:36:50 -0300

    tratando a exception customizada de validações programáticas

--- algafood-api/src/main/java/com/algaworks/algafood/core/validation/ValorZeroIncluiDescricaoValidator.java
+++ algafood-api/src/main/java/com/algaworks/algafood/core/validation/ValorZeroIncluiDescricaoValidator.java
@@ -1,7 +1,5 @@
 package com.algaworks.algafood.core.validation;
 
-import java.math.BigDecimal;
-
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import javax.validation.ValidationException;
