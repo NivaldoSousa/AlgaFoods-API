e1fb5f821eaa936969f7cd02d1644c6b40458d24
Nivaldo Sousa <nillsousa026@gmail.com>
2022-07-03 11:33:56 -0300

    implementação de pesquisas costumizadas e implemntação da paginação utilizando Pegeable

--- algafood-api/src/main/java/com/algaworks/algafood/domain/repository/filter/PedidoFilter.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/repository/filter/PedidoFilter.java
@@ -1,4 +1,22 @@
 package com.algaworks.algafood.domain.repository.filter;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.OffsetDateTime;
+
+@Setter
+@Getter
 public class PedidoFilter {
+
+    private Long clienteId;
+
+    private Long restauranteId;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) // Informa o tipo de formato que tera a data ex: "2000-10-31T01:30:00.000-05:00"
+    private OffsetDateTime dataCriacaoInicio;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) // Informa o tipo de formato que tera a data ex: "2000-10-31T01:30:00.000-05:00
+    private OffsetDateTime dataCriacaoFim;
 }
---
+++ algafood-api/src/main/java/com/algaworks/algafood/infrastructure/repository/spec/PedidoSpec.java
@@ -0,0 +1,39 @@
+package com.algaworks.algafood.infrastructure.repository.spec;
+
+import com.algaworks.algafood.domain.model.Pedido;
+import com.algaworks.algafood.domain.repository.filter.PedidoFilter;
+import org.springframework.data.jpa.domain.Specification;
+
+import javax.persistence.criteria.Predicate;
+import java.util.ArrayList;
+
+public class PedidoSpec {
+
+    public static Specification<Pedido> usandoFiltro(PedidoFilter filtro){
+        return (root, query, builder) -> {
+            //atribuindo o fetch para nao ter prblemas de muitos selects sendo feitos para retornar os registros
+            root.fetch("restaurante").fetch("cozinha");
+            root.fetch("cliente");
+
+            var predicates = new ArrayList<Predicate>(); // criando um array de Predicates para add os mapeamento dos atributos
+
+            if(filtro.getClienteId() != null){
+                predicates.add(builder.equal(root.get("cliente"), filtro.getClienteId())); //mapeando o atributo da classe PedidoFilter com o atributo da classe Pedido
+            }
+
+            if(filtro.getRestauranteId() != null){
+                predicates.add(builder.equal(root.get("restaurante"), filtro.getRestauranteId()));
+            }
+
+            if(filtro.getDataCriacaoInicio() != null){
+                predicates.add(builder.greaterThanOrEqualTo(root.get("dataCriacao"), filtro.getDataCriacaoInicio())); // dataCriacaoInicio tem quer ser maior que dataCriacao
+            }
+
+            if(filtro.getDataCriacaoFim() != null){
+                predicates.add(builder.lessThanOrEqualTo(root.get("dataCriacao"), filtro.getDataCriacaoFim())); // dataCriacaoFim tem quer ser menor que dataCriacao
+            }
+
+            return builder.and(predicates.toArray(new Predicate[0]));
+        };
+    }
+}
