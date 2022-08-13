0b10a38f58ebb102b8c35d0335cf551380994bc4
Nivaldo Sousa <nillsousa026@gmail.com>
2022-08-13 11:55:12 -0300

    Implementando recuperação de foto no serviço de armazenagem

--- algafood-api/src/main/java/com/algaworks/algafood/domain/service/FotoStorageService.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/service/FotoStorageService.java
@@ -2,6 +2,7 @@ package com.algaworks.algafood.domain.service;
 
 import lombok.Builder;
 import lombok.Getter;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.InputStream;
 import java.util.UUID;
@@ -9,6 +10,19 @@ import java.util.UUID;
 public interface FotoStorageService {
 
     void armazenar(NovaFoto novaFoto);
+
+    void remover(String nomeArquivo);
+
+    InputStream recuperar (String nomeArquivo);
+
+    default void substituir(String nomeArquivoAntigo, NovaFoto novaFoto){
+
+        this.armazenar(novaFoto);
+
+        if(StringUtils.isNotEmpty(nomeArquivoAntigo)){
+            this.remover(nomeArquivoAntigo);
+        }
+    }
 
     /*
     * Gera um codigo unico para ser concatenado com o nome da foto
--- algafood-api/src/main/java/com/algaworks/algafood/infrastructure/service/storage/LocalFotoStorageService.java
+++ algafood-api/src/main/java/com/algaworks/algafood/infrastructure/service/storage/LocalFotoStorageService.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.FileCopyUtils;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -21,13 +23,41 @@ public class LocalFotoStorageService implements FotoStorageService {
     public void armazenar(NovaFoto novaFoto) {
 
         try {
-            //caminho da pasta onde será salvo
+            //caminho da pasta + nome arquivo onde será salvo
             Path arquivoPath = getArquivoPath(novaFoto.getNomeArquivo());
 
             //apartir do inputStream sera copiado para o caminho do path
             FileCopyUtils.copy(novaFoto.getInputStream(), Files.newOutputStream(arquivoPath));
         } catch (Exception e) {
           throw new StorageException("Não foi possível armazenar o arquivo", e);
+        }
+    }
+
+    @Override
+    public void remover(String nomeArquivo) {
+
+        try {
+            //caminho da pasta + nome arquivo onde será removido
+            Path arquivoPath = getArquivoPath(nomeArquivo);
+
+            //remove o arquivo apartir do path contendo o caminho mais o nome da foto
+            Files.deleteIfExists(arquivoPath);
+        } catch (Exception e) {
+            throw new StorageException("Não foi possível excluir o arquivo", e);
+        }
+    }
+
+    @Override
+    public InputStream recuperar(String nomeArquivo) {
+
+        try {
+            //caminho da pasta + nome arquivo onde será removido
+            Path arquivoPath = getArquivoPath(nomeArquivo);
+
+            //criar um novo inputStream buscando o arquivo apartir do path
+            return Files.newInputStream(arquivoPath);
+        } catch (Exception e) {
+            throw new StorageException("Não foi possível recuperar o arquivo", e);
         }
     }
 
