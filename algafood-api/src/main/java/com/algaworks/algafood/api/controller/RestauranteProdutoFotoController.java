94935a8a23d23af0593f15448c7dcf47abcb9e03
Nivaldo Sousa <nillsousa026@gmail.com>
2022-08-14 18:33:46 -0300

    Checando media type ao servir arquivos de fotos

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/RestauranteProdutoFotoController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/RestauranteProdutoFotoController.java
@@ -3,7 +3,6 @@ package com.algaworks.algafood.api.controller;
 import com.algaworks.algafood.api.assembler.FotoProdutoModelAssembler;
 import com.algaworks.algafood.api.model.FotoProdutoModel;
 import com.algaworks.algafood.api.model.input.FotoProdutoInput;
-import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.model.FotoProduto;
 import com.algaworks.algafood.domain.model.Produto;
@@ -14,12 +13,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 /*
  * End-Point para o serviço de upload da foto
@@ -44,7 +45,8 @@ public class RestauranteProdutoFotoController {
      * consumes -> desta forma o metodo esta apenas mapeado para o content-type MultipartFile
      * */
     @PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public FotoProdutoModel atualizarFoto(@PathVariable Long restauranteId, @PathVariable Long produtoId, @Valid FotoProdutoInput fotoProdutoInput) throws IOException {
+    public FotoProdutoModel atualizarFoto(@PathVariable Long restauranteId, @PathVariable Long produtoId,
+                                          @Valid FotoProdutoInput fotoProdutoInput) throws IOException {
 
         Produto produto = cadastroProdutoService.buscarOuFalhar(restauranteId, produtoId);
 
@@ -69,21 +71,41 @@ public class RestauranteProdutoFotoController {
         return fotoProdutoModelAssembler.toModel(fotoProduto);
     }
 
-    @GetMapping(produces = MediaType.IMAGE_JPEG_VALUE)
-    public ResponseEntity<InputStreamResource> servirFoto(@PathVariable Long restauranteId, @PathVariable Long produtoId) {
+    /*
+     * @RequeRequestHeader -> essa anotação é possivel especificar o accept obtendo o seu valor da requisição atribuindo a variavel acceptHeader
+     * */
+    @GetMapping
+    public ResponseEntity<InputStreamResource> servirFoto(@PathVariable Long restauranteId, @PathVariable Long produtoId,
+                                                          @RequestHeader(name = "accept") String acceptHeader) throws HttpMediaTypeNotAcceptableException {
 
-       try {
-           FotoProduto fotoProduto = catalogoFotoProdutoService.buscarOuFalhar(restauranteId, produtoId);
+        try {
+            FotoProduto fotoProduto = catalogoFotoProdutoService.buscarOuFalhar(restauranteId, produtoId);
 
-           InputStream inputStream = fotoStorageService.recuperar(fotoProduto.getNomeArquivo());
+            //pegando o tipo de mediaType da foto armazenada
+            MediaType mediaTypeFoto = MediaType.parseMediaType(fotoProduto.getContentType());
+            //recebe a lista de mediaTypes da requisição
+            List<MediaType> mediaTypesAceitas = MediaType.parseMediaTypes(acceptHeader);
 
-           //retorna no corpo da requisição um InputStreamResource apartir do inputStream -> a foto que esta armazenada
-           return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(new InputStreamResource(inputStream));
-       }catch (EntidadeNaoEncontradaException e){
-           //caso nao tenha a foto dando uma exception no buscar ou falhar sera capturada a exception para informar o usuario
-           //que nao foi encontrada a foto 404 not found
-           return ResponseEntity.notFound().build();
-       }
+            verificarCompatibilidadeMediaType(mediaTypeFoto, mediaTypesAceitas);
+
+            InputStream inputStream = fotoStorageService.recuperar(fotoProduto.getNomeArquivo());
+
+            //retorna no corpo da requisição um InputStreamResource apartir do inputStream -> a foto que esta armazenada
+            return ResponseEntity.ok().contentType(mediaTypeFoto).body(new InputStreamResource(inputStream));
+        } catch (EntidadeNaoEncontradaException e) {
+            //caso nao tenha a foto dando uma exception no buscar ou falhar sera capturada a exception para informar o usuario
+            //que nao foi encontrada a foto 404 not found
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    private void verificarCompatibilidadeMediaType(MediaType mediaTypeFoto, List<MediaType> mediaTypesAceitas) throws HttpMediaTypeNotAcceptableException {
+        //verifica se a lista mediaTypeAceitas ao menos uma é copativel com o tipo da foto
+        boolean compativel = mediaTypesAceitas.stream().anyMatch(mediaTypesAceita -> mediaTypesAceita.isCompatibleWith(mediaTypeFoto));
+
+        if (!compativel) {
+            throw new HttpMediaTypeNotAcceptableException(mediaTypesAceitas);
+        }
     }
 
 }
