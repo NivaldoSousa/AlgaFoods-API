614fd8354c7cddd907822d8b26d3bc97a317a9eb
Nivaldo Sousa <nillsousa026@gmail.com>
2023-04-26 21:06:05 -0300

    Configurando o Resource Server com a nova stack do Spring Security

--- algafood-auth/src/main/java/com/algaworks/algafoodauth/AuthorizationServerConfig.java
+++ algafood-auth/src/main/java/com/algaworks/algafoodauth/AuthorizationServerConfig.java
@@ -1,11 +1,46 @@
 package com.algaworks.algafoodauth;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
 @Configuration
 @EnableAuthorizationServer // estamos habilitando o AuthorizationServer nesse projeto que seria por exemplo os endpoint como /token, /auth
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("algafood-web")
+                .secret(passwordEncoder.encode("web123"))
+                .authorizedGrantTypes("password") // tipo de fluxo que esse client ira usar
+                .scopes("write", "read")
+                .accessTokenValiditySeconds(60 * 60 * 6) // tempo de expiração do token, nesse exemplo ele vai expirar em 6h. Padrão é 12h
+                .and()
+                .withClient("checktoken")
+                .secret(passwordEncoder.encode("check123"));
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        //security.checkTokenAccess("isAuthenticated()"); //permitir usar o endpoint oauth/check_token informando os dados do client
+        security.checkTokenAccess("permitAll()"); //permitir usar o endpoint oauth/check_token sem a necessidade de informar os dados do client
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager);
+    }
 }
