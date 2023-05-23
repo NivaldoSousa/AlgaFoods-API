0df3b7dbb989ad7eb4d88d964dbb8fffdb54e20c
Nivaldo Sousa <nillsousa026@gmail.com>
2023-05-23 20:11:49 -0300

    Adicionando claims customizadas no payload do JWT

--- algafood-auth/src/main/java/com/algaworks/algafoodauth/core/AuthorizationServerConfig.java
+++ algafood-auth/src/main/java/com/algaworks/algafoodauth/core/AuthorizationServerConfig.java
@@ -16,6 +16,7 @@ import org.springframework.security.oauth2.provider.CompositeTokenGranter;
 import org.springframework.security.oauth2.provider.TokenGranter;
 import org.springframework.security.oauth2.provider.approval.ApprovalStore;
 import org.springframework.security.oauth2.provider.approval.TokenApprovalStore;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
@@ -103,10 +104,16 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+
+        // Instanciando uma lista de TokenEnhancers
+        var enhancerChain = new TokenEnhancerChain();
+        enhancerChain.setTokenEnhancers(Arrays.asList(new JwtCustomClaimsTokenEnhancer(), jwtAccessTokenConverter()));
+
         endpoints.authenticationManager(authenticationManager)
                 .userDetailsService(userDetailsService) // necessário para utilizar o refresh token
                 .reuseRefreshTokens(false) // não permitir utilizar o refresh token mais de um vez
                 .accessTokenConverter(jwtAccessTokenConverter()) // Configuramos o conversor de Access Token em JWT
+                .tokenEnhancer(enhancerChain) // configurando o Claims
                 .approvalStore(approvalStore(endpoints.getTokenStore()))
                 .tokenGranter(tokenGranter(endpoints));
     }
