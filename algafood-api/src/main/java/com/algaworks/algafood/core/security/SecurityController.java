ccaf0d75eb36f3f9b0999caecb2d63360cdc73be
Nivaldo Sousa <nillsousa026@gmail.com>
2023-08-07 19:49:52 -0300

    Customizando a pagina de aprovação de scope

--- algafood-api/src/main/java/com/algaworks/algafood/core/security/SecurityController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/core/security/SecurityController.java
@@ -13,4 +13,9 @@ public class SecurityController {
     public String login() {
         return "pages/login";
     }
+
+    @GetMapping("/oauth/confirm_access")
+    public String approval(){
+        return "pages/approval";
+    }
 }
