f4c911d6bad8d4b1c7c8ca0a6686d83adc57734f
Nivaldo Sousa <nillsousa026@gmail.com>
2023-09-17 17:00:07 -0300

    Configurando o Spring Session Data Redis e reslvendo o problema com storage de Authorization Codes

--- algafood-auth/src/main/java/com/algaworks/algafoodauth/core/AuthorizationServerConfig.java
+++ algafood-auth/src/main/java/com/algaworks/algafoodauth/core/AuthorizationServerConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.oauth2.provider.CompositeTokenGranter;
 import org.springframework.security.oauth2.provider.TokenGranter;
 import org.springframework.security.oauth2.provider.approval.ApprovalStore;
 import org.springframework.security.oauth2.provider.approval.TokenApprovalStore;
+import org.springframework.security.oauth2.provider.code.JdbcAuthorizationCodeServices;
 import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
@@ -82,6 +83,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
         endpoints.authenticationManager(authenticationManager)
                 .userDetailsService(userDetailsService) // necessário para utilizar o refresh token
+                .authorizationCodeServices(new JdbcAuthorizationCodeServices(this.dataSource)) // Armazenando os tokens no banco de dados
                 .reuseRefreshTokens(false) // não permitir utilizar o refresh token mais de um vez
                 .accessTokenConverter(jwtAccessTokenConverter()) // Configuramos o conversor de Access Token em JWT
                 .tokenEnhancer(enhancerChain) // configurando o Claims
